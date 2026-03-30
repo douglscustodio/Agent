c0c96f9ad142534ca803333ed140ffdaaf167c1e
douglscustodio <dougls1@gmail.com>
2026-03-30 07:23:58 -0300

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -204,7 +204,7 @@ def rank_signals(
         )
 
     return RankingResult(
-        top=ranked_signals := top,
+        top=(ranked_signals := top),
         total_scored=len(scores),
         total_valid=len(valid_scores),
         watchlist=watchlist_scores,
